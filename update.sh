ae50f7ac60b639f04e4468aa56006b3712a786e7
Tianon Gravi <admwiggin@gmail.com>
2015-12-08 16:01:17 -0800

    Perform more simple housecleaning

--- update.sh
+++ update.sh
@@ -6,8 +6,6 @@ gpgKeys=(
 	[7.0]='1A4E8B7277C42E53DBA9C7B9BCAA30EA9C0D5763'
 	[5.6]='0BD78B5F97500D450838F95DFE857D9A90D90EC1 6E4F6AB321FDC07F2C332E3AC2BF0BC433CFC8B3'
 	[5.5]='0B96609E270F565C13292B24C13C70B87267B52D 0BD78B5F97500D450838F95DFE857D9A90D90EC1 F38252826ACD957EF380D39F2F7956BC5DA04B5D'
-	[5.4]='F38252826ACD957EF380D39F2F7956BC5DA04B5D'
-	[5.3]='0A95E9A026542D53835E3F3A7DEC4E69FC9C83D7 0B96609E270F565C13292B24C13C70B87267B52D'
 )
 # see http://php.net/downloads.php
 
@@ -19,7 +17,7 @@ if [ ${#versions[@]} -eq 0 ]; then
 fi
 versions=( "${versions[@]%/}" )
 
-packagesUrl='http://php.net/releases/index.php?serialize=1&version=5&max=100'
+packagesUrl='http://php.net/releases/index.php?serialize=1&max=100'
 packages="$(echo "$packagesUrl" | sed -r 's/[^a-zA-Z.-]+/-/g')"
 curl -sSL "${packagesUrl}" > "$packages"
 
