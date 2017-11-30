3f132379556d132a6a416cbf34c31901d8eb1afd
Joe Ferguson <joe@infosiftr.com>
2017-11-30 14:26:03 -0800

    Link back to original issue on no s390x for 7.2+

--- generate-stackbrew-library.sh
+++ generate-stackbrew-library.sh
@@ -130,6 +130,8 @@ for version in "${versions[@]}"; do
 			variantArches="${parentRepoToArches[$variantParent]}"
 
 			# 7.2 no longer supports s390x
+			# #error "Not yet implemented"
+			# https://github.com/docker-library/php/pull/487#issue-254755661
 			if [[ "$version" = 7.* ]] && [ "$version" != '7.0' ] && [ "$version" != '7.1' ]; then
 				variantArches="$(echo " $variantArches " | sed -r -e 's/ s390x//g')"
 			fi
