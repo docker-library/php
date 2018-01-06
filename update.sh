06795a60c38d353540ee3459ac3b2667b18bda86
Tianon Gravi <admwiggin@gmail.com>
2018-01-05 17:01:21 -0800

    Add "pollita" on 7.1 (7.1.13 is signed by 1729F83938DA44E27BA0F4D3DBDB397470D12172)

--- update.sh
+++ update.sh
@@ -11,9 +11,10 @@ declare -A gpgKeys=(
 
 	# https://wiki.php.net/todo/php71
 	# davey & krakjoe
+	# pollita for 7.1.13 for some reason
 	# https://secure.php.net/downloads.php#gpg-7.1
 	# https://secure.php.net/gpg-keys.php#gpg-7.1
-	[7.1]='A917B1ECDA84AEC2B568FED6F50ABC807BD5DCD0 528995BFEDFBA7191D46839EF9BA0ADA31CBD89E'
+	[7.1]='A917B1ECDA84AEC2B568FED6F50ABC807BD5DCD0 528995BFEDFBA7191D46839EF9BA0ADA31CBD89E 1729F83938DA44E27BA0F4D3DBDB397470D12172'
 
 	# https://wiki.php.net/todo/php70
 	# ab & tyrael
