828493b2a090a29135e0721a33b682d3cdf842ca
Blue Gaston <bbluegaston@gmail.com>
2018-11-15 11:25:53 -0800

    reorg

--- 7.2/stretch/apache/scripts/build-scrambled.sh
+++ 7.2/stretch/apache/scripts/build-scrambled.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-if [ ! -f "$PATH/s_php" ]; then
-        cp /usr/local/bin/php /usr/local/bin/s_php
-fi
-
 ./php-scrambler
 
 s_php tok-php-transformer.php -p "$PHP_SRC_PATH"ext/phar --replace --inc --phar
--- 7.2/stretch/apache/scripts/scramble.sh
+++ 7.2/stretch/apache/scripts/scramble.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+if [ ! -f "$PATH/s_php" ]; then
+        cp /usr/local/bin/php /usr/local/bin/s_php
+fi
+
+
 if [ "$MODE" == "poly" ] 
 then
 	echo "SCRAM"
 	cd $POLYSCRIPT_PATH
 	./build-scrambled.sh
 	s_php tok-php-transformer.php -p /usr/src/wordpress --replace
-	rm -rf /usr/bin/s_php
 fi
+
