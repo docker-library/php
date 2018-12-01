796819b770fe048695521827fa79ce42ce12d18a
Blue Gaston <bbluegaston@gmail.com>
2018-12-01 09:23:30 -0800

    snip-transfrom.sh no longer necessary, replacing pharcommand.inc no longer necessary

--- 7.2/stretch/apache/scripts/build-scrambled.sh
+++ 7.2/stretch/apache/scripts/build-scrambled.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
+  
+./php-scrambler
+
+cd $PHP_SRC_PATH
+make install -k
 
 cd $POLYSCRIPT_PATH
+s_php tok-php-transformer.php -p "$PHP_SRC_PATH"ext/phar --replace --inc --phar
 
-./php-scrambler
-if [ -f ./scrambled.json ]; then 
-        s_php tok-php-transformer.php -p "$PHP_SRC_PATH"ext/phar --replace --inc --phar
-        cd $PHP_SRC_PATH
-        make install -k
-else 
-        exit 1
-fi
+cd $PHP_SRC_PATH
+make install -k
 
 cd $POLYSCRIPT_PATH
 s_php tok-php-transformer.php -p "$PHP_SRC_PATH"ext/phar/phar.php --replace
+
 cd $PHP_SRC_PATH
 make pharcmd -k
--- 7.2/stretch/apache/scripts/snip-transform.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ -f /php/scrambled.json ]; then     
-	out="$(/php/php /php/tok-php-transformer.php -s "$SNIP")"
-	echo "$out"
-else
-        echo "$SNIP"
-fi
-
